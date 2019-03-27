9f0072e06ec6304fbbf3341b53973ff864036a8b
Carlos Catarino <43734195+CarlosKta27@users.noreply.github.com>
2019-03-26 20:56:48 -0600

    Menu y alta

--- funciones.h
+++ funciones.h
@@ -1,6 +1,62 @@
 #ifndef ARBOLES_FUNCIONES_H
 #define ARBOLES_FUNCIONES_H
 
-typedef struct
+typedef struct _Nodo{
+    int dato;
+    struct _Nodo *L;
+    struct _Nodo *R;
+}Nodo;
+
+Nodo* allocateMem(int dato)
+{
+    Nodo* dummy;
+    dummy = (Nodo*)malloc(sizeof(Nodo));
+    dummy -> dato = dato;
+    dummy -> R = NULL;
+    dummy -> L = NULL;
+    return dummy;
+}
+
+Nodo *alta(Nodo *top,int dato)
+{
+    Nodo *aux,*nuevo;
+    nuevo = allocateMem(dato);
+    aux = top;
+    if(aux == NULL)
+    {
+        return nuevo;
+    }
+    else
+    {
+        while(aux!=NULL)
+        {
+            if(aux->dato<nuevo->dato)
+            {
+                if(aux->R==NULL)
+                {
+                    aux->R=nuevo;
+                    break;
+                }
+                else
+                {
+                    aux = aux->R;
+                }
+            }
+            else
+            {
+                if(aux->L==NULL)
+                {
+                    aux->L=nuevo;
+                    break;
+                }
+                else
+                {
+                    aux = aux->L;
+                }
+            }
+        }
+    }
+    return top;
+}
 
 #endif
--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "funciones.h"
 
 int main() {
-    printf("Hello, World!\n");
+    int m;
+    do{
+        puts("ARBOLES SIN FRUTOS\n"
+             "1 >> Altas\n"
+             "2 >> Bajas\n"
+             "3 >> Mostrar");
+        scanf("%d",&m);
+        switch (m)
+        {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+
+                break;
+
+            case 0:
+                puts("CHI CHENOL");
+                break;
+
+            default: puts("Inserte una opcion valida");
+        }
+    }while(m!=0);
     return 0;
 }
